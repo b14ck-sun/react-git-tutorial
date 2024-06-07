e55f301d50db317cbe72233e8b8e6f0a60c18e5b
Aiden <aiden@bordergiant,com>
2024-06-07 14:49:03 -0400

    imported from template

--- chatbot-react/src/App.jsx
+++ chatbot-react/src/App.jsx
@@ -1,14 +1,55 @@
-import { useState } from 'react'
-function App() {
-  const [count, setCount] = useState(0)
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+
+import BotMessage from "./components/BotMessage";
+import UserMessage from "./components/UserMessage";
+import Messages from "./components/Messages";
+import Input from "./components/Input";
+
+import API from "./ChatbotAPI";
+
+import "./App.css";
+import Header from "./components/Header";
+
+function Chatbot() {
+  const [messages, setMessages] = useState(["Hello World"]);
+
+  useEffect(() => {
+    async function loadWelcomeMessage() {
+      setMessages([
+        <BotMessage
+          key="0"
+          fetchMessage={async () => await API.GetChatbotResponse("hi")}
+        />
+      ]);
+    }
+    loadWelcomeMessage();
+  }, []);
+
+  const send = async text => {
+    const newMessages = messages.concat(
+      <UserMessage key={messages.length + 1} text={text} />,
+      <BotMessage
+        key={messages.length + 2}
+        fetchMessage={async () => await API.GetChatbotResponse(text)}
+      />
+    );
+    setMessages(newMessages);
+  };
 
   return (
-    <>
-      <div>
-        Hellow
-      </div>
-    </>
-  )
+    <div className="chatbot">
+      <Header />
+      <Messages messages={messages} />
+      <Input onSend={send} />
+    </div>
+  );
 }
 
-export default App
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<Chatbot />, rootElement);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Chatbot />
+  </React.StrictMode>,
+)
